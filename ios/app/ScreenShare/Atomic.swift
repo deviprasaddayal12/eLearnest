7426cb38da3d26b3cc474f4b65c17bc2bf5dae41
deviprasaddayal12 <deviprasaddayal12@gmail.com>
2021-12-24 15:53:06 +0530

    screen share fixed, rls v1.5

---
+++ ios/app/ScreenShare/Atomic.swift
@@ -0,0 +1,37 @@
+//
+//  Atomic.swift
+//  ScreenShare
+//
+//  Created by Dayal Das on 20/12/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+struct Atomic<Value> {
+
+    private var value: Value
+    private let lock = NSLock()
+
+    init(wrappedValue value: Value) {
+        self.value = value
+    }
+
+    var wrappedValue: Value {
+      get { return load() }
+      set { store(newValue: newValue) }
+    }
+
+    func load() -> Value {
+        lock.lock()
+        defer { lock.unlock() }
+        return value
+    }
+
+    mutating func store(newValue: Value) {
+        lock.lock()
+        defer { lock.unlock() }
+        value = newValue
+    }
+}
---
+++ ios/app/ScreenShare/SampleHandler.swift
@@ -0,0 +1,147 @@
+//
+//  SampleHandler.swift
+//  ScreenShare
+//
+//  Created by Dayal Das on 20/12/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+//import ReplayKit
+//
+//class SampleHandler: RPBroadcastSampleHandler {
+//
+//    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+//        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+//    }
+//
+//    override func broadcastPaused() {
+//        // User has requested to pause the broadcast. Samples will stop being delivered.
+//    }
+//
+//    override func broadcastResumed() {
+//        // User has requested to resume the broadcast. Samples delivery will resume.
+//    }
+//
+//    override func broadcastFinished() {
+//        // User has requested to finish the broadcast.
+//    }
+//
+//    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+//        switch sampleBufferType {
+//        case RPSampleBufferType.video:
+//            // Handle video sample buffer
+//            break
+//        case RPSampleBufferType.audioApp:
+//            // Handle audio sample buffer for app audio
+//            break
+//        case RPSampleBufferType.audioMic:
+//            // Handle audio sample buffer for mic audio
+//            break
+//        @unknown default:
+//            // Handle other sample buffer types
+//            fatalError("Unknown type of sample buffer")
+//        }
+//    }
+//}
+
+import ReplayKit
+import JitsiMeetSDK
+
+private enum Constants {
+    // the App Group ID value that the app and the broadcast extension targets are setup with. It differs for each app.
+    static let appGroupIdentifier = "group.com.elearnest.appgroup"
+}
+
+class SampleHandler: RPBroadcastSampleHandler {
+    
+    private var clientConnection: SocketConnection?
+    private var uploader: SampleUploader?
+    
+    private var frameCount: Int = 0
+    
+    var socketFilePath: String {
+      let sharedContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Constants.appGroupIdentifier)
+        
+        return sharedContainer?.appendingPathComponent("rtc_SSFD").path ?? ""
+    }
+    
+    override init() {
+      super.init()
+        if let connection = SocketConnection(filePath: socketFilePath) {
+          clientConnection = connection
+          setupConnection()
+          
+          uploader = SampleUploader(connection: connection)
+        }
+    }
+
+    override func broadcastStarted(withSetupInfo setupInfo: [String: NSObject]?) {
+        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+        print("broadcast started")
+        
+        frameCount = 0
+        
+        DarwinNotificationCenter.shared.postNotification(.broadcastStarted)
+        openConnection()
+    }
+    
+    override func broadcastPaused() {
+        // User has requested to pause the broadcast. Samples will stop being delivered.
+    }
+    
+    override func broadcastResumed() {
+        // User has requested to resume the broadcast. Samples delivery will resume.
+    }
+    
+    override func broadcastFinished() {
+        // User has requested to finish the broadcast.
+        DarwinNotificationCenter.shared.postNotification(.broadcastStopped)
+        clientConnection?.close()
+    }
+    
+    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+        switch sampleBufferType {
+        case RPSampleBufferType.video:
+            // very simple mechanism for adjusting frame rate by using every third frame
+            frameCount += 1
+            if frameCount % 3 == 0 {
+                uploader?.send(sample: sampleBuffer)
+            }
+        default:
+            break
+        }
+    }
+}
+
+private extension SampleHandler {
+  
+    func setupConnection() {
+        clientConnection?.didClose = { [weak self] error in
+            print("client connection did close \(String(describing: error))")
+          
+            if let error = error {
+                self?.finishBroadcastWithError(error)
+            } else {
+                // the displayed failure message is more user friendly when using NSError instead of Error
+                let JMScreenSharingStopped = 10001
+                let customError = NSError(domain: RPRecordingErrorDomain, code: JMScreenSharingStopped, userInfo: [NSLocalizedDescriptionKey: "Screen sharing stopped"])
+                self?.finishBroadcastWithError(customError)
+            }
+        }
+    }
+    
+    func openConnection() {
+        let queue = DispatchQueue(label: "broadcast.connectTimer")
+        let timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.schedule(deadline: .now(), repeating: .milliseconds(100), leeway: .milliseconds(500))
+        timer.setEventHandler { [weak self] in
+            guard self?.clientConnection?.open() == true else {
+                return
+            }
+            
+            timer.cancel()
+        }
+        
+        timer.resume()
+    }
+}
